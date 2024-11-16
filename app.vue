1e977ea790e74f8f15e5c45c971c8d5819a329ae
Alexandr <80260446+UralDeveloper@users.noreply.github.com>
2024-11-16 23:20:28 +0500

    upd

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <NuxtLayout name="default">
+      <Header />
       <NuxtRouteAnnouncer />
       <NuxtPage />
     </NuxtLayout>
---
+++ components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <div class="header-container container">
+            <div class="header-logo">
+                <a href="/">Лого</a>
+            </div>
+            <div class="header-menu">
+                <ul>
+                    <li><a href="#"># Элемент</a></li>
+                    <li><a href="#"># Элемент</a></li>
+                    <li><a href="#"># Элемент</a></li>
+                    <li><a href="#"># Элемент</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
