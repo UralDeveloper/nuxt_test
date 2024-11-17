33cb23f5de4c7e6720543348d86445be6f1dc696
Alexandr <80260446+UralDeveloper@users.noreply.github.com>
2024-11-17 23:54:34 +0500

    UPD

--- app.vue
+++ app.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-  import { SpeedInsights } from "@vercel/speed-insights/nuxt"
-</script>
 <template>
   <div class="app">
     <NuxtLayout name="default">
-      <SpeedInsights/>
       <LayoutHeader />
       <NuxtLoadingIndicator />
       <NuxtRouteAnnouncer />
